8e4eb13f75a497c731e5c2128f4f206589d07075
Pekka Koukkula <pekka.koukkula@genisys.fi>
2019-02-27 12:43:54 +0200

    Initial commit

---
+++ Assets/Scripts/EditorScripts/EditorSnap.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode] [SelectionBase]
+public class EditorSnap : MonoBehaviour
+{
+    [Range(1f, 20f)] [SerializeField] float gridSize = 10f;
+    TextMesh numberLocation;
+
+    private void Start()
+    {
+        numberLocation = gameObject.GetComponentInChildren<TextMesh>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / 10f) * gridSize;
+        snapPos.y = Mathf.RoundToInt(transform.position.y / 10f) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / 10f) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
+
+        string labelText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        numberLocation.text = labelText;
+        gameObject.name = "Waypoint " + labelText;
+    }
+}
