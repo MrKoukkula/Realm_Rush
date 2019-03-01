3a329231b83f21bb86d0cf8f1163e186ac47d13b
PekkaKoukkula <pekka.koukkula@genisys.fi>
2019-03-01 20:56:53 +0200

    pathfinding almost done

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 public class PathFinder : MonoBehaviour
 {
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Queue<Waypoint> queue = new Queue<Waypoint>();
+    bool isRunning = true;
+    Waypoint searchCenter;
+
     [SerializeField] Waypoint startWaypoint, endWaypoint;
     Vector2Int[] directions =
     {
@@ -18,20 +22,70 @@ public class PathFinder : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LoadBlocks();
-        addStartAndEnd();
-        ExploreNeightbours();
+        LoadBlocks(); //Load the world
+        addStartAndEnd(); //Mark the start and end points
+        PathFind(); //Start searching
     }
 
-    private void ExploreNeightbours()
+    private void PathFind()
     {
-        foreach(Vector2Int direction in directions)
+        queue.Enqueue(startWaypoint); //Because the queue is empty, addthe start point
+
+        while (queue.Count > 0 && isRunning) // As long as the queue is not empty, this loop will run.
         {
-            Vector2Int explorationPos = startWaypoint.GetGridPos + direction;
-            print("Exploring " + explorationPos);
-            grid[explorationPos].SetTopColor(Color.blue);
+            searchCenter = queue.Dequeue(); // pop the first item from the queue
+            print("Searching from " + searchCenter);
+            HaltIfEndFound(); //Check if the item is the endpoint
+            // explore neightbours
+            ExploreNeightbours(); //If the item is not the endpoint, look and add neighbours to queue
+            searchCenter.isExplored = true;
+        }
+        // todo work-out the best path
+    }
+
+    private void ExploreNeightbours() //Check for nodes in neighbouring squares
+    {
+        if (!isRunning) { return; };
+
+        foreach(Vector2Int direction in directions) // Check the 4 directions of the current searchpoint
+        {
+            Vector2Int explorationPos = searchCenter.GetGridPos + direction; //Search direction is up, right, down, left
+            //print("Exploring " + explorationPos);
+            try
+            {
+                queueNewNeighbour(explorationPos);
+            }
+            catch
+            {
+                // Do nothing
+            }
+        }
+    }
+
+    private void queueNewNeighbour(Vector2Int explorationPos)
+    {
+        Waypoint neighbour = grid[explorationPos]; //Find the item from the dictionary and place it into the queue
+        if (neighbour.isExplored || queue.Contains(neighbour))
+        {
+            
+        } else
+        {
+            //neighbour.SetTopColor(Color.blue);
+            neighbour.exploredFrom = searchCenter;
+            queue.Enqueue(neighbour);
+            print("queuing from " + neighbour);
         }
         
+    }
+
+    private void HaltIfEndFound() // This only checks if the current waypoint if the end
+    {
+        if (searchCenter == endWaypoint)
+        {
+            print("Endpoint found");
+            isRunning = false;
+            return;
+        }
     }
 
     private void LoadBlocks()
