61fa172bd8c12492db4eb7d4baa353c2a42046b6
Pekka Koukkula <pekka.koukkula@genisys.fi>
2019-03-12 14:06:21 +0200

    Added enemy spawning coroutine

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] EnemyMovement enemy;
+    [SerializeField] GameObject enemySpawningPoint;
+    [SerializeField] float secondsBetweenSpawns;
+    [SerializeField] int amountOfEnemies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("spawnEnemy");
+    }
+
+
+    IEnumerator spawnEnemy()
+    {
+        while (amountOfEnemies != 0)
+        {
+            Instantiate(enemy, enemySpawningPoint.transform);
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+            amountOfEnemies--;
+        }
+        
+    }
+}
