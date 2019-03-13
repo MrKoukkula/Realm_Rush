007dbf497ff347b6999e9cc891d113f5ef27ce50
Pekka Koukkula <pe.koukkula@gmail.com>
2019-03-13 10:36:30 +0200

    Mouse over blocks logging

---
+++ Assets/Scripts/MouseOver.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOver : MonoBehaviour
+{
+    private void OnMouseOver()
+    {
+        print("Eek! A Mouse is on "+ transform.position);
+    }
+}
