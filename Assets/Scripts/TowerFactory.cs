7349975ae19f01a53bf95f4a9ea2501f14991495
Pekka Koukkula <pe.koukkula@gmail.com>
2019-03-14 13:58:58 +0200

    Starting to build ring buffer

---
+++ Assets/Scripts/TowerFactory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerFactory : MonoBehaviour
+{
+    [SerializeField] int towerLimit = 5;
+    [SerializeField] Tower towerPrefab;
+
+    Queue<Tower> ringBuffer = new Queue<Tower>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
